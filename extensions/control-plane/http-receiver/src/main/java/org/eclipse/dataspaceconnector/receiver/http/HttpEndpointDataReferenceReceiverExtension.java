e2618a024b51cc34bb9249ed42724b0770de18fc
Dr. Christoph "Schorsch" Jung <c-jung@t-systems.com>
2022-09-15 13:36:48 +0200

    https://jira.catena-x.net/browse/KA-39 Better way to introduce multiple endpoint callbacks with different authentication settings.

--- extensions/control-plane/http-receiver/src/main/java/org/eclipse/dataspaceconnector/receiver/http/HttpEndpointDataReferenceReceiverExtension.java
+++ extensions/control-plane/http-receiver/src/main/java/org/eclipse/dataspaceconnector/receiver/http/HttpEndpointDataReferenceReceiverExtension.java
@@ -19,6 +19,7 @@ import okhttp3.OkHttpClient;
 import org.eclipse.dataspaceconnector.common.string.StringUtils;
 import org.eclipse.dataspaceconnector.spi.EdcException;
 import org.eclipse.dataspaceconnector.spi.EdcSetting;
+import org.eclipse.dataspaceconnector.spi.monitor.Monitor;
 import org.eclipse.dataspaceconnector.spi.system.Inject;
 import org.eclipse.dataspaceconnector.spi.system.ServiceExtension;
 import org.eclipse.dataspaceconnector.spi.system.ServiceExtensionContext;
@@ -34,6 +35,8 @@ public class HttpEndpointDataReferenceReceiverExtension implements ServiceExtens
 
     @EdcSetting
     private static final String HTTP_RECEIVER_AUTH_CODE = "edc.receiver.http.auth-code";
+
+    public static final String SPEC_SEPARATOR=",";
 
     @Inject
     private EndpointDataReferenceReceiverRegistry receiverRegistry;
@@ -58,15 +61,46 @@ public class HttpEndpointDataReferenceReceiverExtension implements ServiceExtens
         }
         var authKey = context.getSetting(HTTP_RECEIVER_AUTH_KEY, null);
         var authCode = context.getSetting(HTTP_RECEIVER_AUTH_CODE, null);
-        var receiver = HttpEndpointDataReferenceReceiver.Builder.newInstance()
-                .endpoint(endpoint)
-                .authHeader(authKey, authCode)
-                .httpClient(httpClient)
-                .typeManager(context.getTypeManager())
-                .retryPolicy(retryPolicy)
-                .monitor(context.getMonitor())
-                .build();
-        receiverRegistry.registerReceiver(receiver);
+
+        Monitor monitor= context.getMonitor();
+
+        monitor.debug(String.format("Elaborating http endpoint receiver spec %s",endpoint));
+
+        var endpoints=endpoint.split(",");
+        var authKeys= new String[0];
+        if(authKey!=null) {
+            authKeys=authKey.split(",");
+        }
+        var authCodes= new String[0];
+        if(authCode!=null) {
+            authCodes=authCode.split(",");
+        }
+
+        if(authKeys.length!=authCodes.length) {
+            throw new EdcException(String.format("Length of endpoints %d auth keys %d and auth codes %d do not match", endpoints.length,authKeys.length,authCodes.length));
+        }
+        if(authKeys.length<endpoints.length) {
+            monitor.warning(String.format("There are %d endpoints but only %d authorization infos. Omitting authorization for endpoints %d to %d.",endpoints.length,authKeys.length,authKeys.length+1,endpoints.length));
+        }
+        for(int count=0;count<endpoints.length;count++) {
+            monitor.debug(String.format("About to build and register new http endpoint receiver number %d for address %s",count,endpoints[count]));
+            String theEndpoint=endpoints[count];
+            String theKey="";
+            String theCode="";
+            if(authKeys.length>count) {
+                theKey=authKeys[count];
+                theCode=authCodes[count];
+            }
+            var receiver = HttpEndpointDataReferenceReceiver.Builder.newInstance()
+                    .endpoint(theEndpoint)
+                    .authHeader(theKey, theCode)
+                    .httpClient(httpClient)
+                    .typeManager(context.getTypeManager())
+                    .retryPolicy(retryPolicy)
+                    .monitor(monitor)
+                    .build();
+            receiverRegistry.registerReceiver(receiver);
+        }
     }
 
 }
